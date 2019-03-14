f7ef9f1cf8869905204b8dc868becb9254933750
S$onu <32143858+indsonu@users.noreply.github.com>
2019-03-14 19:57:23 +0530

    Update Vowel_Combination_CODECHEF.cpp

--- Vowel_Combination_CODECHEF.cpp
+++ Vowel_Combination_CODECHEF.cpp
@@ -19,6 +19,7 @@ while(t--)
         else if(s[i][j]=='e'){a[1]=1;}
         else if(s[i][j]=='i'){a[2]=1;}                       //                    1   0   1   0    1
         else if(s[i][j]=='o'){a[3]=1;}                       //                    0   1   0   1    0       OR of these 2 is:
+                                                             //                    ------------------
         else if(s[i][j]=='u'){a[4]=1;}                       //                    1   1   1   1    1 
         }
         d=a[0]*1+a[1]*2+a[2]*4+a[3]*8+a[4]*16;
@@ -27,7 +28,7 @@ while(t--)
         long long int c=0;
         for (auto k=1; k<=30;k++) {
                 for (auto l=k+1; l<32;l++) {
-                    if (((k|l)==31)&&b[k]!=0&&b[l]!=0) { c=c+(b[k]*b[l]);}}}
+                    if (((k|l)==31)&&b[k]!=0&&b[l]!=0) { c=c+(b[k]*b[l]);}}}        (k | l)==31 checking bits
                 long long int r=b[31];
                 c=c+(r*(r-1)/2);
                 cout<<c<<"\n";
